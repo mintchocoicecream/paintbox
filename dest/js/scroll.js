00031a867291cc19618252e4e738d394104f536d
mintchocoicecream <sxxhnjng@gmail.com>
2022-08-20 17:12:19 +0900

    add js

---
+++ dest/js/scroll.js
@@ -0,0 +1,18 @@
+const getHeader = document.getElementById("header");
+const logo = document.getElementById("logo");
+console.log(getHeader);
+
+function scrollFunction(){
+    if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
+        getHeader.style.height = "75px";
+        getHeader.style.transition = ".3s ease-in-out";
+        logo.style.width = "5em";
+      } else {
+        getHeader.style.height = "150px";
+        getHeader.style.transition = ".3s ease-in-out";
+        logo.style.width = "8em";
+        logo.style.transition = ".3s ease-in-out";
+      }
+}
+
+window.onscroll = function() {scrollFunction()};
